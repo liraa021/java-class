1974c21d56bf6ec145da121b8413cc635bb0a89b
Rodrigo Lira <rodrigolira@gmail.com>
2026-06-09 20:39:29 -0300

    feat: adicionado spring-web e endpoints help e health

--- src/main/java/com/rodrigolira/meu_primeiro_projeto/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/rodrigolira/meu_primeiro_projeto/MeuPrimeiroProjetoApplication.java
@@ -1,13 +1,28 @@
 package com.rodrigolira.meu_primeiro_projeto;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
-@SpringBootApplication
-public class MeuPrimeiroProjetoApplication {
+@RestController
+public class RestControllerAPI {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
-	}
+    @GetMapping("/help")
+    public Map<String, Object> getHelpInfo() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "UP");
+        response.put("app_name", "meu-primeiro-projeto");
+        response.put("version", "1.0.0");
+        response.put("environment", "AWS EC2 - Ubuntu");
+        response.put("description", "API Spring Boot integrada com sucesso na infraestrutura Cloud.");
+        return response;
+    }
 
+    @GetMapping("/health")
+    public Map<String, String> getHealth() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "UP");
+        return response;
+    }
 }
